8c5527ca1073cc18c9e7cac5bece773c96943821
joaovictor3g <jvdias79797@gmail.com>
2020-05-10 18:56:30 -0300

    database configuration file

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        client: 'mysql',
+        connection: {
+            host : '127.0.0.1',
+            user : 'joao',
+            password : 'root',
+            database : 'testeDB'
+        }
+    }
+};
