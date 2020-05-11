8fa32bccc7297cbeccd5c7772855d3b1fc3206a7
joaovictor3g <jvdias79797@gmail.com>
2020-05-10 21:15:16 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,15 @@ const express = require('express');
 const routes = express.Router();
 
 const studentController = require('./controllers/addStudent');
+const profesorController = require('./controllers/addProfesor')
 
-routes.post('/', studentController.addStudent);
-routes.get('/view', studentController.viewAllStudents);
+routes.get('/', (req, res) => {
+    return res.send('Hello World');
+});
+routes.post('/add-new-student', studentController.addStudent);
+routes.get('/view-students', studentController.viewAllStudents);
+routes.post('/add-new-professor', profesorController.addNewProfessor);
+routes.get('/view-profesors', profesorController.viewAllProfessor);
+
 
 module.exports = routes;
